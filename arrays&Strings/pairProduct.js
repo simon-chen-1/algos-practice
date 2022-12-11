d0e27d3a9fc934c354c4298d8e5166c354467070
Simon Chen <simonchn160@gmail.com>
2022-12-11 15:22:05 -0500

    pairProduct solution

---
+++ arrays&Strings/pairProduct.js
@@ -0,0 +1,30 @@
+// Link to problem: https://structy.net/problems/premium/pair-product
+
+// Approach:
+// We are trying to find two numbers that multiply to the target. So, we can iterate through the 
+// array of numbers and divide the current number from the target to find its complement. Then,
+// we can check inside our complements object to see if we've seen this complement before. If not, 
+// set the current number as a key in the object and its value as the current index. If we have,
+// return an array with the current index and the value (the index) of the complement from the object.
+
+// Solution:
+const pairProduct = (numbers, targetProduct) => {
+  const complements = {};
+  
+  for (let i = 0; i < numbers.length; i++) {
+    const complement = targetProduct / numbers[i];
+    if (complement in complements) {
+      return [i, complements[complement]];
+    } else {
+      complements[numbers[i]] = i;
+    }
+  }
+};
+
+// Time Complexity:
+// O(N) where N represents the length of the numbers array. We're just iterating throught the array once
+// and there's no repeated work
+
+// Space Complexity:
+// O(N) where N represents the length of the numbers array. In the worst case scenario (if there are
+// no pairs that multiply to the target), we'll end up storing all the numbers as keys in our object.
